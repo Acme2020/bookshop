2c498df541b2c5272f5c0caed90096590d9ff714
Acme2020 <stefandrescher82@googlemail.com>
2021-12-03 10:47:37 +0100

    Added book test names and corrected minor spelling mistakes

--- bookshop/src/test/BookTest.java
+++ bookshop/src/test/BookTest.java
@@ -3,6 +3,7 @@ package test;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import bookshop.Book;
@@ -11,6 +12,7 @@ class BookTest
 {
 
     @Test
+    @DisplayName("If an input to create a book is missing or wrong an exception should be thrown")
     void testNoInputException()
     {
         Exception exception = assertThrows(IllegalArgumentException.class, () ->
@@ -21,6 +23,7 @@ class BookTest
     }
 
     @Test
+    @DisplayName("If a book with an unknown genre is created an exception should be thrown")
     void testWrongGenreException()
     {
         Exception exception = assertThrows(IllegalArgumentException.class, () ->
